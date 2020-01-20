7249aa70d80161a0fb66610d26ffc2446697d5da
AliaksandrHaurylenka <goric0312@mail.ru>
2020-01-20 16:20:29 +0300

    asd

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
-if (6*8 == 48) {
-  console.log("Да!");
-}
+"use strict";
+
+// Задание 1
+let money = "1200";
+let time = "2020-01-20";
+
+let appData = {
+  budget: money,
+  timeData: time,
+  expenses: {question1: "", question2: ""},
+  optionalExpenses: "",
+  income: [],
+  savings: false
+};
+
+
+let age = prompt("Ваш бюджет на месяц?", appData.budget);
+
+alert(`Тебе ${age} лет!`); // Тебе 100 лет!
