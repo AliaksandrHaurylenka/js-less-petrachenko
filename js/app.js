93a708d40557abdca2d43101f771334a9919c442
AliaksandrHaurylenka <goric0312@mail.ru>
2020-01-21 16:25:04 +0300

    asd

--- js/app.js
+++ js/app.js
@@ -1,19 +1,83 @@
 "use strict";
 
 // Задание 1
-let money = "1200";
-let time = "2020-01-20";
+// let money = 1200;
+// let time = "2020-01-20";
+
+// let appData = {
+//   budget: money,
+//   timeData: time,
+//   expenses: {question1: "Оплатить комуналку", question2: "150 рублей"},
+//   optionalExpenses: "",
+//   income: [],
+//   savings: false
+// };
+
+// console.log(typeof(appData));
+// console.log(appData);
+// console.log(appData.expenses.question1 + ':' + appData.expenses.question2);
+
+// let budgetOneDay = money/30;
+// alert("Ваш бюджет должен составлять " + budgetOneDay + " рублей в день!");
+//Конец Задание 1
+
+
+//Задание 2
+// let x = 5; alert( x++ );
+// console.log([ ] + false - null + true);
+// let y = 1; let x = y = 2; alert(x);
+// console.log([ ] + 1 + 2);
+// alert( "1"[0] );
+// console.log(2 && 1 && null && 0 && undefined);
+// console.log(!!( a && b ));
+// console.log(( a && b ));
+// alert( null || 2 && 3 || 4 );
+// alert( +"Infinity" );
+// console.log(0 || "" || 2 || undefined || true || falsе);
+// Конец Задание 2
+
+
+// Задание 3
+let money = prompt('Ваш бюджет на месяц?', '');
+let time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
 let appData = {
-  budget: money,
-  timeData: time,
-  expenses: {question1: "", question2: ""},
-  optionalExpenses: "",
-  income: [],
-  savings: false
-};
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: "",
+    income: [],
+    savings: false
+  };
 
 
-let age = prompt("Ваш бюджет на месяц?", appData.budget);
+for (let i = 0; i < 2; i++) {
+  let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+      b = prompt("Во сколько обойдется?", '');
 
-alert(`Тебе ${age} лет!`); // Тебе 100 лет!
+  if ((typeof(a) != null) && 
+      (typeof(b) != null) && 
+      a != "" && 
+      b != ""
+    ) {
+    appData.expenses[a] = b;
+  } 
+}
+console.log(appData);
+
+
+appData.moneyPerDay = appData.budget / 30;
+alert(appData.moneyPerDay);
+
+if (appData.moneyPerDay < 20) {
+  console.log ("У Вас минимальный уровень достатка");
+} else if (appData.moneyPerDay > 20 && appData.moneyPerDay < 50) {
+  console.log ("У Вас средний уровень достатка");
+} else if (appData.moneyPerDay > 50) {
+  console.log ("У Вас высокий уровень достатка");
+} else {
+  console.log('Что-то пошло не так!');
+}
+// Конец Задание 3
+
+
