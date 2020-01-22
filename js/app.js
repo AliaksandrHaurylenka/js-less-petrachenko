962716f8f2c03ddd9fb3f9f1d35dd046762f5440
AliaksandrHaurylenka <goric0312@mail.ru>
2020-01-22 09:02:21 +0300

    home3

--- js/app.js
+++ js/app.js
@@ -51,7 +51,36 @@ let appData = {
   };
 
 
-for (let i = 0; i < 2; i++) {
+// for (let i = 0; i < 2; i++) {
+//   let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+//       b = prompt("Во сколько обойдется?", '');
+
+//   if ((typeof(a) != null) && 
+//       (typeof(b) != null) && 
+//       a != "" && 
+//       b != ""
+//     ) {
+//     appData.expenses[a] = b;
+//   } 
+// }
+
+// let i = 0;
+// while (i < 2) {
+//   let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+//       b = prompt("Во сколько обойдется?", '');
+
+//   if ((typeof(a) != null) && 
+//       (typeof(b) != null) && 
+//       a != "" && 
+//       b != ""
+//     ) {
+//     appData.expenses[a] = b;
+//   }
+//   i++;
+// }
+
+let i = 0;
+do {
   let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
       b = prompt("Во сколько обойдется?", '');
 
@@ -61,8 +90,10 @@ for (let i = 0; i < 2; i++) {
       b != ""
     ) {
     appData.expenses[a] = b;
-  } 
-}
+  }
+  i++;
+} while (i < 2);
+
 console.log(appData);
 
 
