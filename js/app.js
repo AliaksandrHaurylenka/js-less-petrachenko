0a5f7bfb892b78d591654abc4624c3296bf7b5e0
AliaksandrHaurylenka <goric0312@mail.ru>
2020-01-22 16:24:39 +0300

    home4

--- js/app.js
+++ js/app.js
@@ -38,17 +38,17 @@
 
 
 // Задание 3
-let money = prompt('Ваш бюджет на месяц?', '');
-let time = prompt('Введите дату в формате YYYY-MM-DD', '');
+// let money = prompt('Ваш бюджет на месяц?', '');
+// let time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
-let appData = {
-    budget: money,
-    timeData: time,
-    expenses: {},
-    optionalExpenses: "",
-    income: [],
-    savings: false
-  };
+// let appData = {
+//     budget: money,
+//     timeData: time,
+//     expenses: {},
+//     optionalExpenses: "",
+//     income: [],
+//     savings: false
+//   };
 
 
 // for (let i = 0; i < 2; i++) {
@@ -79,25 +79,82 @@ let appData = {
 //   i++;
 // }
 
-let i = 0;
-do {
-  let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
-      b = prompt("Во сколько обойдется?", '');
+// let i = 0;
+// do {
+//   let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+//       b = prompt("Во сколько обойдется?", '');
 
-  if ((typeof(a) != null) && 
-      (typeof(b) != null) && 
-      a != "" && 
-      b != ""
-    ) {
-    appData.expenses[a] = b;
+//   if ((typeof(a) != null) && 
+//       (typeof(b) != null) && 
+//       a != "" && 
+//       b != ""
+//     ) {
+//     appData.expenses[a] = b;
+//   }
+//   i++;
+// } while (i < 2);
+
+// console.log(appData);
+
+
+// appData.moneyPerDay = appData.budget / 30;
+// alert(appData.moneyPerDay);
+
+// if (appData.moneyPerDay < 20) {
+//   console.log ("У Вас минимальный уровень достатка");
+// } else if (appData.moneyPerDay > 20 && appData.moneyPerDay < 50) {
+//   console.log ("У Вас средний уровень достатка");
+// } else if (appData.moneyPerDay > 50) {
+//   console.log ("У Вас высокий уровень достатка");
+// } else {
+//   console.log('Что-то пошло не так!');
+// }
+// Конец Задание 3
+
+
+//Задание №4
+let money, time;
+
+function start() {
+  let money = +prompt('Ваш бюджет на месяц?', '');
+  while((isNaN(money) || money == '' || money == null)) {
+    money = +prompt('Ваш бюджет на месяц?', '');
   }
-  i++;
-} while (i < 2);
 
+  let time = prompt('Введите дату в формате YYYY-MM-DD', '');
+}
+start();
+
+let appData = {
+  budget: money,
+  timeData: time,
+  expenses: {},
+  optionalExpenses: "",
+  income: [],
+  savings: false
+};
 console.log(appData);
 
+function chooseExpenses() {
+  for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+        b = +prompt("Во сколько обойдется?", '');
+  
+    if ((typeof(a) != null) && 
+        (typeof(b) != null) && 
+        a != "" && 
+        b != ""
+      ) {
+      appData.expenses[a] = b;
+    } 
+  }
+}
+chooseExpenses();
 
-appData.moneyPerDay = appData.budget / 30;
+
+
+
+appData.moneyPerDay = (appData.budget / 30).toFixed();
 alert(appData.moneyPerDay);
 
 if (appData.moneyPerDay < 20) {
@@ -109,6 +166,6 @@ if (appData.moneyPerDay < 20) {
 } else {
   console.log('Что-то пошло не так!');
 }
-// Конец Задание 3
+//Конец Задание №4
 
 
