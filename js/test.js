5eeb52746f15f33a28dd2784d74d0a02ba8e6149
AliaksandrHaurylenka <goric0312@mail.ru>
2020-02-11 15:52:20 +0300

    asd

--- js/test.js
+++ js/test.js
@@ -1,4 +1,15 @@
-let arr = [1, 19, 6],
-    i = arr.sort();
+// let arr = [1, 19, 6],
+//     i = arr.sort();
 
-console.log(arr);
+// console.log(arr);
+
+function Calculator(){
+  this.read = function(){
+    this.a = +prompt('a?');
+    this.b = +prompt('b?');
+  }
+  
+}
+
+let calc = new Calculator();
+alert(calc.read());
