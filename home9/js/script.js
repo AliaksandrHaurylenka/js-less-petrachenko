4cee939952d7988a436c198881979ee437b8d383
AliaksandrHaurylenka <goric0312@mail.ru>
2020-02-14 14:01:48 +0300

    asd

--- home9/js/script.js
+++ home9/js/script.js
@@ -91,49 +91,23 @@ window.addEventListener('DOMContentLoaded', function(){
   setClock('timer', deadline);
 
   // Modal
-  // let more = document.querySelector('.more'),
-  //     close = document.querySelector('.popup-close'),
-  //     overlay = document.querySelector('.overlay');
-
-  // more.addEventListener('click', function(){
-  //   overlay.style.display = 'block';
-  //   this.classList.add('more-splash');
-  //   document.body.style.overflow = 'hidden';
-  // });
-
-  // close.addEventListener('click', function(){
-  //   overlay.style.display = 'none';
-  //   more.classList.remove('more-splash');
-  //   document.body.style.overflow = '';
-  // });
-
   function More(btn, btnClose){
-    this.btn = document.querySelector('.' + btn);
-    this.close = document.querySelector('.' + btnClose);
-    // this.overlay = document.querySelector('.overlay');
+    let self = this;
+    self.btn = document.querySelector('.' + btn);
+    self.close = document.querySelector('.' + btnClose);
+    self.overlay = document.querySelector('.overlay');
 
-    // console.log(this.overlay);
+    // console.log(self.overlay);
 
-
-    this.btn.addEventListener('click', function(){
-      function displayBlock(){
-        let overlay = document.querySelector('.overlay');
-        overlay.style.display = 'block';
-      }
-      displayBlock();
+    self.btn.addEventListener('click', function(){
+      self.overlay.style.display = 'block';
       this.classList.add('more-splash');
       document.body.style.overflow = 'hidden';
     });
 
-    this.close.addEventListener('click', function(){
-      function displayNone(btn){
-        let overlay = document.querySelector('.overlay'),
-            button = document.querySelector('.' + btn);       
-        
-        overlay.style.display = 'none';
-        button.classList.remove('more-splash');
-      }
-      displayNone(btn);
+    self.close.addEventListener('click', function(){
+      self.overlay.style.display = 'none';
+      self.btn.classList.remove('more-splash');
       document.body.style.overflow = '';
     });
   }
