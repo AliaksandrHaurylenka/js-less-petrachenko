d988a372a1788aa3cba609a876fd8d016f33df79
AliaksandrHaurylenka <goric0312@mail.ru>
2020-02-17 15:39:25 +0300

    asd

--- home9/js/script.js
+++ home9/js/script.js
@@ -104,7 +104,7 @@ window.addEventListener('DOMContentLoaded', function(){
       this.cssAnimation = cssAnimation;
       this.btn.onclick = this.show.bind(this);
       this.close.onclick = this.hide.bind(this);
-      console.log(this.btn);
+      // console.log(this.btn);
       // console.log(this.cssAnimation);
     }
 
@@ -121,33 +121,8 @@ window.addEventListener('DOMContentLoaded', function(){
       document.body.style.overflow = '';
     }   
   }
-  // class More {
-  //   constructor(btn) {
-  //     this.btn = document.querySelector('.' + btn);
-  //     this.close = document.querySelector('.popup-close');
-  //     this.overlay = document.querySelector('.overlay');
-  //     this.btn.onclick = this.show.bind(this);
-  //     this.close.onclick = this.hide.bind(this);
-  //     // console.log(this.btn);
-  //     // console.log(this.cssAnimation);
-  //   }
-
-  //   show(){
-  //     this.overlay.style.display = 'block';
-  //     this.btn.classList.add('more-splash');
-  //     document.body.style.overflow = 'hidden';
-  //   }
-
-  //   hide(){
-  //     this.overlay.style.display = 'none';
-  //     this.btn.classList.remove('more-splash');
-  //     document.body.style.overflow = '';
-  //   }   
-  // }
-  // console.log(More);
+  
   let tabSection = new More('description-btn', 'popup-close', 'overlay', 'more-splash');
   let timerSection = new More('more', 'popup-close', 'overlay', 'more-splash');
-  // let tabSection = new More('description-btn');
-  // let timerSection = new More('more');
 
 });
