0993cfa3a27e23dd1155197b965123ad219d5ae6
Aliaksandr Haurylenka <aliaksandr.haurylenka@gmail.com>
2020-02-18 18:15:40 +0300

    asd

--- home9/js/script.js
+++ home9/js/script.js
@@ -192,45 +192,65 @@ window.addEventListener('DOMContentLoaded', function(){
       this.input = this.form.getElementsByTagName('input');
       this.statusMessage = document.createElement('div');
       this.statusMessage.classList.add('status');
+      this.form.appendChild(this.statusMessage);
   
       this.form.onsubmit = this.toSend.bind(this);
+
+      this.request = new XMLHttpRequest();
+      this.request.onreadystatechange = this.message.bind(this);
     }
-  
-    toSend(){
+
+    /**
+     * Отправка сообщения
+     */
+    toSend() {
       event.preventDefault();
+      this.requestJson();
+      // this.requestFormData();
+      this.message();
+      this.inputClear();
+    }
 
-      this.form.appendChild(this.statusMessage);
-
-      let request = new XMLHttpRequest();
-      request.open('POST', 'server.php');
-
-      //Отправка формы в обычном формате
-      // request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-      // let formData = new FormData(this.form);
-      // request.send(formData);
-      // Конец Отправка формы в обычном формате
-
-      //Отправка формы в json формате
-      request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+    //Отправка формы в json формате
+    requestJson() {
+      // let request = new XMLHttpRequest();
+      this.request.open('POST', 'server.php');
+      this.request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
       let formData = new FormData(this.form);
       let obj = {};
       formData.forEach(function(value, key){
         obj[key] = value;
       });
       let json = JSON.stringify(obj);
-      request.send(json);
-      //Конец Отправка формы в json формате
+      this.request.send(json);
+    }
 
-      request.addEventListener('readystatechange', () => {
-        if(request.readyState < 4){
-          this.statusMessage.innerHTML = this.loading;
-        } else if(request.readyState === 4 && request.status == 200){
-          this.statusMessage.innerHTML = this.success;
-        } else {
-          this.statusMessage.innerHTML = this.failure;
-        }
-      });
+    //Отправка формы в обычном формате
+    requestFormData(){
+      // let request = new XMLHttpRequest();
+      this.request.open('POST', 'server.php');
+      this.request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+      let formData = new FormData(this.form);
+      this.request.send(formData);
+    }
 
+    /**
+     * Вывод сообщения при отправке в зависимости от статуса
+     */
+    message() {
+      if(this.request.readyState < 4){
+        this.statusMessage.innerHTML = this.loading;
+      } else if(this.request.readyState === 4 && this.request.status == 200){
+        this.statusMessage.innerHTML = this.success;
+      } else {
+        this.statusMessage.innerHTML = this.failure;
+      }
+    }
+
+    /**
+     * Очистка полей после отправки
+     */
+    inputClear() {
       for(let i = 0; i < this.input.length; i++){
         this.input[i].value = '';
       }
