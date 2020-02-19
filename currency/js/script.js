9dbbf0b4ae7891b7132467d5b0e74471c3e311d6
AliaksandrHaurylenka <goric0312@mail.ru>
2020-02-19 16:27:37 +0300

    asd

---
+++ currency/js/script.js
@@ -0,0 +1,69 @@
+// let inputRub = document.getElementById('rub'),
+//     inputUsd = document.getElementById('usd');
+
+// inputRub.addEventListener('input', () => {
+//     let request = new XMLHttpRequest();
+
+//     request.open('GET', 'js/current.json');
+//     request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+//     request.send();
+    
+//     request.addEventListener('readystatechange', function() {
+//         if (request.readyState === 4 && request.status == 200) {
+//             let data = JSON.parse(request.response);
+
+//             inputUsd.value = inputRub.value / data.usd;
+//         } else {
+//             inputUsd.value = "Что-то пошло не так!";
+//         }
+//     });
+
+// });
+
+let inputRub = document.getElementById('rub'),
+    inputUsd = document.getElementById('usd');
+
+inputRub.addEventListener('input', () => {
+    function inputRub(){
+        return new Promise (function(resolve, reject){
+            let request = new XMLHttpRequest();
+
+            request.open('GET', 'js/current.json');
+            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+            let data = JSON.parse(request.response);
+            console.log(data);
+
+            request.onreadystatechange = () => {
+                if (request.readyState === 4 && request.status == 200) {
+                    resolve();
+                } else {
+                    reject();
+                }
+            }
+
+            request.send();
+        })
+    }
+    
+    inputRub()
+            .then(() => {
+                // let data = JSON.parse(request.response);
+                inputUsd.value = inputRub.value / data.usd;
+            })
+            .catch(() => {
+                console.log(data);
+                inputUsd.value = "Что-то пошло не так!";
+            })
+   
+    
+    // request.addEventListener('readystatechange', function() {
+    //     if (request.readyState === 4 && request.status == 200) {
+    //         let data = JSON.parse(request.response);
+
+    //         inputUsd.value = inputRub.value / data.usd;
+    //     } else {
+    //         inputUsd.value = "Что-то пошло не так!";
+    //     }
+    // });
+
+});
