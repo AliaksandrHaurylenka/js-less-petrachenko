3718724da093256a6a15b4ee470a95a8f10377e8
Aliaksandr Haurylenka <aliaksandr.haurylenka@gmail.com>
2020-02-25 07:19:42 +0300

    asd

--- Homework/script.js
+++ Homework/script.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
-  $('#tour').on('click', function(){
 
+  $('a[href="#tour"], .main_btn, a[href="#sheldure"]').on('click', function(){
+    // console.log(this);
+    $('.overlay, .modal').fadeToggle();
+  });
+
+  $('.close').on('click', function(){
+    $('.overlay, .modal').fadeToggle();
   });
 });
